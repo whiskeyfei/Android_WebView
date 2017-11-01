f3577e800a61c74f7688ce315f802956d0c86fa7
CaoPengfei <whiskeyfei@users.noreply.github.com>
2017-11-01 23:35:18 +0800

    init

---
+++ app/src/main/java/com/webview/MainActivity.java
@@ -0,0 +1,321 @@
+package com.webview;
+
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.ConsoleMessage;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private SafeWebView mWebView;
+    private LinearLayout mRoot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mRoot = (LinearLayout) findViewById(R.id.main);
+        //实际使用时请采用 new 的方式
+        mWebView = (SafeWebView) findViewById(R.id.web_view);
+        //4.2 开启辅助功能崩溃
+        mWebView.disableAccessibility(getApplicationContext());
+        initWebSettings();
+        initListener();
+        initinject();
+//        mWebView.loadUrl("http://www.jianshu.com/u/fa272f63280a");
+        mWebView.loadUrl("file:///android_asset/test.html");
+    }
+
+    private void initinject() {
+        mWebView.addJavascriptInterface(new NativeInterface(this), "AndroidNative");
+    }
+
+    private void initWebSettings() {
+        WebSettings webSettings = mWebView.getSettings();
+        if (webSettings == null) return;
+        // 支持 Js 使用
+        webSettings.setJavaScriptEnabled(true);
+        // 开启DOM缓存
+        webSettings.setDomStorageEnabled(true);
+        // 开启数据库缓存
+        webSettings.setDatabaseEnabled(true);
+        // 支持自动加载图片
+        webSettings.setLoadsImagesAutomatically(hasKitkat());
+        // 设置 WebView 的缓存模式
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        // 支持启用缓存模式
+        webSettings.setAppCacheEnabled(true);
+        // 设置 AppCache 最大缓存值(现在官方已经不提倡使用，已废弃)
+        webSettings.setAppCacheMaxSize(8 * 1024 * 1024);
+        // Android 私有缓存存储，如果你不调用setAppCachePath方法，WebView将不会产生这个目录
+        webSettings.setAppCachePath(getCacheDir().getAbsolutePath());
+        // 数据库路径
+        if (!hasKitkat()) {
+            webSettings.setDatabasePath(getDatabasePath("html").getPath());
+        }
+        // 关闭密码保存提醒功能
+        webSettings.setSavePassword(false);
+        // 支持缩放
+        webSettings.setSupportZoom(true);
+        // 设置 UserAgent 属性
+        webSettings.setUserAgentString("");
+        // 允许加载本地 html 文件/false
+        webSettings.setAllowFileAccess(true);
+        // 允许通过 file url 加载的 Javascript 读取其他的本地文件,Android 4.1 之前默认是true，在 Android 4.1 及以后默认是false,也就是禁止
+        webSettings.setAllowFileAccessFromFileURLs(false);
+        // 允许通过 file url 加载的 Javascript 可以访问其他的源，包括其他的文件和 http，https 等其他的源，
+        // Android 4.1 之前默认是true，在 Android 4.1 及以后默认是false,也就是禁止
+        // 如果此设置是允许，则 setAllowFileAccessFromFileURLs 不起做用
+        webSettings.setAllowUniversalAccessFromFileURLs(false);
+
+        /**
+         *  关于缓存目录:
+         *
+         *  我自测发现以下规律，如果你有涉及到目录操作，需要自己做下验证。
+         *  Android 4.4 以下：/data/data/包名/cache
+         *  Android 4.4 - 5.0：/data/data/包名/app_webview/cache/
+         */
+    }
+
+    private void initListener() {
+        mWebView.setWebViewClient(new SafeWebViewClient());
+        mWebView.setWebChromeClient(new SafeWebChromeClient());
+    }
+
+    public class SafeWebViewClient extends WebViewClient {
+
+        /**
+         * 当WebView得页面Scale值发生改变时回调
+         */
+        @Override
+        public void onScaleChanged(WebView view, float oldScale, float newScale) {
+            super.onScaleChanged(view, oldScale, newScale);
+        }
+
+        /**
+         * 是否在 WebView 内加载页面
+         *
+         * @param view
+         * @param url
+         * @return
+         */
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        /**
+         * WebView 开始加载页面时回调，一次Frame加载对应一次回调
+         *
+         * @param view
+         * @param url
+         * @param favicon
+         */
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        /**
+         * WebView 完成加载页面时回调，一次Frame加载对应一次回调
+         *
+         * @param view
+         * @param url
+         */
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+
+        /**
+         * WebView 加载页面资源时会回调，每一个资源产生的一次网络加载，除非本地有当前 url 对应有缓存，否则就会加载。
+         *
+         * @param view WebView
+         * @param url  url
+         */
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            super.onLoadResource(view, url);
+        }
+
+        /**
+         * WebView 可以拦截某一次的 request 来返回我们自己加载的数据，这个方法在后面缓存会有很大作用。
+         *
+         * @param view    WebView
+         * @param request 当前产生 request 请求
+         * @return WebResourceResponse
+         */
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+            return super.shouldInterceptRequest(view, request);
+        }
+
+        /**
+         * WebView 访问 url 出错
+         *
+         * @param view
+         * @param request
+         * @param error
+         */
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+        }
+
+        /**
+         * WebView ssl 访问证书出错，handler.cancel()取消加载，handler.proceed()对然错误也继续加载
+         *
+         * @param view
+         * @param handler
+         * @param error
+         */
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            super.onReceivedSslError(view, handler, error);
+        }
+    }
+
+    public class SafeWebChromeClient extends WebChromeClient {
+
+        @Override
+        public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+            return super.onConsoleMessage(consoleMessage);
+        }
+
+        /**
+         * 当前 WebView 加载网页进度
+         *
+         * @param view
+         * @param newProgress
+         */
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+        }
+
+        /**
+         * Js 中调用 alert() 函数，产生的对话框
+         *
+         * @param view
+         * @param url
+         * @param message
+         * @param result
+         * @return
+         */
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            return super.onJsAlert(view, url, message, result);
+        }
+
+        /**
+         * 处理 Js 中的 Confirm 对话框
+         *
+         * @param view
+         * @param url
+         * @param message
+         * @param result
+         * @return
+         */
+        @Override
+        public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+            return super.onJsConfirm(view, url, message, result);
+        }
+
+        /**
+         * 处理 JS 中的 Prompt对话框
+         *
+         * @param view
+         * @param url
+         * @param message
+         * @param defaultValue
+         * @param result
+         * @return
+         */
+        @Override
+        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+            return super.onJsPrompt(view, url, message, defaultValue, result);
+        }
+
+        /**
+         * 接收web页面的icon
+         *
+         * @param view
+         * @param icon
+         */
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+        }
+
+        /**
+         * 接收web页面的 Title
+         *
+         * @param view
+         * @param title
+         */
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebView.onResume();
+        mWebView.resumeTimers();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mWebView.onPause();
+        mWebView.pauseTimers();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mRoot != null) {
+            mRoot.removeView(mWebView);
+        }
+        if (mWebView != null) {
+            mWebView.stopLoading();
+            mWebView.clearMatches();
+            mWebView.clearHistory();
+            mWebView.clearSslPreferences();
+            mWebView.clearCache(true);
+            mWebView.loadUrl("about:blank");
+            mWebView.removeAllViews();
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                mWebView.removeJavascriptInterface("AndroidNative");
+            }
+            mWebView.destroy();
+        }
+        mWebView = null;
+    }
+
+    private static boolean hasKitkat() {
+        return Build.VERSION.SDK_INT >= 19;
+    }
+}
---
+++ app/src/main/java/com/webview/NativeInterface.java
@@ -0,0 +1,32 @@
+package com.webview;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+
+public class NativeInterface {
+
+    private Context mContext;
+
+    public NativeInterface(Context context) {
+        mContext = context;
+    }
+
+    @JavascriptInterface
+    public void hello() {
+        Toast.makeText(mContext, "hello", Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public void hello(String params) {
+        Toast.makeText(mContext, params, Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public String getAndroid() {
+        Toast.makeText(mContext, "getAndroid", Toast.LENGTH_SHORT).show();
+        return "Android data";
+    }
+
+}
